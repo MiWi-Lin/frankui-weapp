4ba270b34970b06234b2253ae24a3829c1f51a3d
Miwi <lmw@feibo.cn>
2018-10-09 18:08:54 +0800

    缓存限制 & auto add & auto remove

--- components/horizontal-day-select/horizontal-day-select.js
+++ components/horizontal-day-select/horizontal-day-select.js
@@ -4,11 +4,15 @@ Component({
    * 组件的属性列表
    */
   properties: {
-    moreCount: { // 滑动到顶或底后，每次添加的item个数
+    moreCount: { // 滑动到顶或底后，每次添加的item个数, 为了计算itemWidth应大于屏幕显示个数
       type: Number,
       value: 30
     },
     maxCacheCount: { // 同时存在内存中的日期最大个数
+      type: Number,
+      value: 100
+    },
+    threshold: { // 触发Add的距离
       type: Number,
       value: 100
     },
@@ -34,13 +38,35 @@ Component({
     selectedDate: null,
     scrollIntoId: '',
     isAnimation: true,
-    scrollLeft: -100
+    scrollLeft: 1, // 设置1可以帮助获得itemWidth
+    viewWidth: 0, // scroll-view屏幕宽度
+    viewPadding: 0, // scroll-view 左右padding
+    scrollData: {
+      left: 0,
+      width: 0,
+      itemWidth: 0,
+      timer: null,
+      isAdded: false
+    }
   },
   /**
    * 组件实例进入页面节点树时执行
    */
   attached() {
     this.onAttached();
+  },
+  ready() {
+    const that = this;
+    const queryA = wx.createSelectorQuery().in(this);
+    queryA.select('#h-scroll-view').boundingClientRect();
+    queryA.exec(function (res) {
+      that.setData({ viewWidth: res[0].width });
+    });
+    const queryB = wx.createSelectorQuery().in(this);
+    queryB.select('#top-padding').boundingClientRect();
+    queryB.exec(function (res) {
+      that.setData({ viewPadding: res[0].width });
+    });
   },
   /**
    * 组件的方法列表
@@ -68,6 +94,8 @@ Component({
     addDays(date, isEnd) {
       let list = this.data.ips;
       let count = this.data.moreCount;
+      let addCount = 0;
+      let removeCount = 0;
       if (isEnd) {
         const minTime = this.data.minDate ? this.dateToZero(this.data.minDate).getTime() : 0;
         for (let x = 0; x < count; x++) {
@@ -77,8 +105,10 @@ Component({
           }
           const item = this.createDayObj(tempDate);
           list.unshift(item);
+          addCount += 1;
         }
         if (list.length > this.data.maxCacheCount) {
+          removeCount = list.length - this.data.maxCacheCount;
           list = list.slice(0, this.data.maxCacheCount);
         }
       } else {
@@ -90,13 +120,16 @@ Component({
           }
           const item = this.createDayObj(tempDate);
           list.push(item);
+          addCount += 1;
         }
         if (list.length > this.data.maxCacheCount) {
+          removeCount = list.length - this.data.maxCacheCount;
           list = list.slice(list.length - this.data.maxCacheCount);
         }
       }
       this.setData({ ips: list });
-      console.info(`日期总个数：${list.length}`);
+      console.info(`日期总个数：${list.length} -${removeCount} +${addCount}`);
+      return { addCount, removeCount };
     },
 
     createDayObj(date) {
@@ -130,27 +163,83 @@ Component({
       this.setData({ scrollIntoId });
     },
 
-    handleToupper() {
+    setScrollLeft(scrollLeft) {
+      this.setData({ scrollLeft });
+    },
+    handleToupper(e) {
+      const { scrollLeft } = e.detail;
       this.setIsAnimation(false);
       console.info('handleToupper');
-      // const firstDayId = this.data.ips[0].id;
-      this.addDays(this.getBeforeDay(this.data.ips[0].date), true);
-      // this.setScrollIntoId(firstDayId);
+      const { addCount, removeCount } = this.addDays(this.getBeforeDay(this.data.ips[0].date), true);
+      // this.setScrollLeft(addCount * this.data.scrollData.itemWidth + scrollLeft - this.data.viewPadding);
+      this.setScrollLeft(addCount * this.data.scrollData.itemWidth + scrollLeft);
     },
 
-    handleTolower() {
-      this.setIsAnimation(true);
+    handleTolower(e) {
+      const { scrollLeft, scrollWidth, deltaX } = e.detail;
+      this.setIsAnimation(false);
       console.info('handleTolower');
-      // const lastDayId = this.data.ips[this.data.ips.length - 1].id;
+      const originalCount = this.data.ips.length;
       const nextDay = this.getNextDay(this.data.ips[this.data.ips.length - 1].date);
-      this.addDays(nextDay, false);
-      // this.setScrollIntoId(lastDayId);
+      const { addCount, removeCount } = this.addDays(nextDay, false);
+      if (removeCount > 0) {
+        this.setScrollLeft(scrollLeft - removeCount * this.data.scrollData.itemWidth);
+      }
     },
 
     handleScroll(e) {
+      if (this.data.scrollData.timer) {
+        clearTimeout(this.data.scrollData.timer);
+        this.data.scrollData.timer = null;
+      }
+
       const { scrollLeft, scrollWidth, deltaX } = e.detail;
       console.info(`HandleScroll: ${scrollLeft}-${scrollWidth}-${deltaX}`);
-      // this.setData({ scrollLeft : 10});
+      this.data.scrollData.left = scrollLeft;
+      this.data.scrollData.width = scrollWidth;
+      if (this.data.scrollData.itemWidth <= 0 && this.data.ips && this.data.ips.length > 0) {
+        this.data.scrollData.itemWidth = (scrollWidth - this.data.viewPadding * 2) / this.data.ips.length;
+      }
+
+      if (this.data.scrollData.isAdded && (scrollLeft < this.data.threshold || scrollWidth - scrollLeft - this.data.viewWidth < this.data.threshold)) {
+        return
+      } else {
+        this.data.scrollData.isAdded = false;
+      }
+
+      if (scrollLeft < this.data.threshold) {
+        if (deltaX >= scrollLeft){
+          e.detail.deltaX += e.detail.scrollLeft;
+          e.detail.scrollLeft = 0;
+        }
+        this.data.scrollData.timer = setTimeout(() => {
+          this.data.scrollData.isAdded = true;
+          this.handleToupper(e);
+        }, 100);
+      } else if (scrollWidth - scrollLeft - this.data.viewWidth < this.data.threshold) {
+        if (deltaX <= -(scrollWidth - scrollLeft - this.data.viewWidth)) {
+          e.detail.deltaX -= scrollWidth - scrollLeft - this.data.viewWidth;
+          e.detail.scrollLeft = scrollWidth - this.data.viewWidth;
+        }
+        this.data.scrollData.timer = setTimeout(() => {
+          this.data.scrollData.isAdded = true;
+          this.handleTolower(e);
+        }, 100);
+      } else if (scrollLeft >= this.data.threshold && deltaX >= this.data.threshold && deltaX >= scrollLeft) {
+        e.detail.deltaX += e.detail.scrollLeft;
+        e.detail.scrollLeft = 0;
+        this.data.scrollData.timer = setTimeout(() => {
+          this.data.scrollData.isAdded = true;
+          this.handleToupper(e);
+        }, 100);
+      } else if (scrollWidth - scrollLeft - this.data.viewWidth >= this.data.threshold && deltaX <= -this.data.threshold && deltaX <= -(scrollWidth - scrollLeft - this.data.viewWidth)) {
+        e.detail.deltaX -= scrollWidth - scrollLeft - this.data.viewWidth;
+        e.detail.scrollLeft = scrollWidth - this.data.viewWidth;
+        this.data.scrollData.timer = setTimeout(() => {
+          this.data.scrollData.isAdded = true;
+          this.handleTolower(e);
+        }, 100);
+      }
     },
     onIpItemClick: function (event) {
       this.setIsAnimation(true);
